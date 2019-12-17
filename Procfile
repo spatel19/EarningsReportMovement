570d1de80a6b99231cb4b8a0c0d8a7334ff369c6
Smeet Patel <smeetpatel@Smeets-MacBook-Pro.local>
2019-12-17 12:16:33 -0500

    Lets get it to heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn flaskfin.py:app
+web: gunicorn flaskfin:app
---
+++ cred.py
@@ -0,0 +1,2 @@
+mongoConnectionString="mongodb+srv://smeetp:letscode11@earningapp-prlhu.mongodb.net/test?retryWrites=true&w=majority"
+API_KEY = "2M4VL8FDSR2RHP7O"
